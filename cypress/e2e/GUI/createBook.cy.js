2fd5750e3eed18eacd4fc6e63f4ab024413a27c9
David Andrade <david1999andrade@gmail.com>
2024-09-21 10:24:28 -0300

    ajust

--- cypress/e2e/GUI/createBook.cy.js
+++ cypress/e2e/GUI/createBook.cy.js
@@ -1,14 +1,21 @@
 describe('Criar um novo Livro', () => {
 
-    it('create new book', () => {
+    beforeEach(() => {
+        cy.fixture('example').then(function (exemple) {
+            this.exemple = exemple
+        })
+    })
+
+    it('create new book', function () {
+        const ex = this.exemple
         cy.visit('http://127.0.0.1:5500/src/frontend/indes.html')
-        cy.get('#title').type('David History')
-        cy.get('#author').type('David Andrade')
-        cy.get('#publisher').type('saraiva')
-        cy.get('#year').type('2024')
-        cy.get('#pages').type(200)
+        cy.get('#title').type(ex.title)
+        cy.get('#author').type(ex.author)
+        cy.get('#publisher').type(ex.publisher)
+        cy.get('#year').type(ex.year)
+        cy.get('#pages').type(ex.pages)
         cy.get('#bookForm > button').click()
-        cy.get('#bookList').contains('David History')
+        cy.get('#bookList').contains(ex.title)
     });
 
 })
